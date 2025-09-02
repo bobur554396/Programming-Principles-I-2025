23e02f447e8bb58696fd8a0b89bea270d030a058
Bobur <bobur.muhsimbaev@gmail.com>
2025-09-02 14:22:24 +0500

    upd

---
+++ week1/examples/informatics/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    double a, b;
+
+    cin >> a >> b;
+
+    double res = sqrt(a * a + b * b);
+
+    cout << res << endl;
+
+    return 0;
+}
---
+++ week1/examples/informatics/b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    // cout << "The next number for the number " << n << " is " << n + 1 << ".\n";
+    cout << "The next number for the number " << n << " is " << n + 1 << "." << endl;
+    cout << "The previous number for the number " << n << " is " << n - 1 << "." << endl;
+    
+
+    return 0;
+}
